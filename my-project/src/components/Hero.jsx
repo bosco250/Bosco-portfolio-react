2654d4b3308c0acd82c526cddf1ec83ea7e3fe56
bosco250 <dusengimana06@gmail.com>
2024-07-18 12:00:25 +0200

    small change

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
             initial={{x:100,opacity:0}}
             animate={{x:0,opacity:1}}
             transition={{duration:1,delay:1.2}}
-            style={{width:"600px",height:"400px"}} src={profilePic} alt="Profile" className=" lg:border-b-4 border-zinc-800 rounded-2xl" />
+            src={profilePic} alt="Profile" className="lg:w-[600px] lg:h-[400px] lg:border-b-4 lg:border-zinc-800 rounded-2xl" />
           </div>
         </div>
       </div>
