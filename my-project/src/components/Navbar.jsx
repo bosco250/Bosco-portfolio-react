577d8f6d2849372336ccae2cd76fdbefa7409405
bosco250 <dusengimana06@gmail.com>
2024-07-20 08:49:03 +0200

    hv

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -9,25 +9,25 @@ function Navbar() {
     <nav className="mb-15 flex items-center justify-between py-6">
       <div className=" flex flex-shrink-0 items-center ">
         {/* <img className='m-8 w-10' src={Logo} alt="logo" /> */}
-        <h1 className=" text-4xl text-neutral-700">BOSCO</h1>
+        <h1 className=" text-4xl text-neutral-700 hover:cursor-pointer">BOSCO</h1>
       </div>
       <div className=" m-6 flex items-center justify-center gap-4 text-2xl">
         <a
-          className=" hover:cursor-pointer hover:text-cyan-500"
+          className=" hover:cursor-pointer hover:text-orange-500"
           href="https://www.linkedin.com/in/dusengimana-jean-bosco-161650296/"
         >
           <FaLinkedin />
         </a>
-        <a className=" hover:cursor-pointer" href="https://github.com/bosco250">
+        <a className=" hover:cursor-pointer hover:text-orange-500" href="https://github.com/bosco250">
           <FaGithub />
         </a>
         <a
-          className=" hover:cursor-pointer hover:text-red-300"
+          className=" hover:cursor-pointer hover:text-orange-500"
           href="https://www.instagram.com/p/C0ynsQ1thuq/?igsh=MWZoZmlkOTN4bGk0Zw=="
         >
           <FaInstagram />
         </a>
-        <a className=" hover:cursor-pointer" href="https://x.com/Jeanbobo250">
+        <a className=" hover:cursor-pointer hover:text-orange-500" href="https://x.com/Jeanbobo250">
           <FaSquareXTwitter />
         </a>
       </div>
